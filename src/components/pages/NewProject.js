387f28e0b12befa773a31867fb78010ac14bf7e0
jackson <jacksondesobral@hotmail.com>
2023-03-17 23:11:51 -0300

    estilização_components

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -9,7 +9,7 @@ function NewProject(){
             </h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
             <p>Formulário</p>
-            <ProjectForm/>
+            <ProjectForm btnText='Criar Projeto'/>
         </div>
     )
 
