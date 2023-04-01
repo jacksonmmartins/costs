e0d16c3fe5a38bb10308071b0f88297c4f47ac52
jackson <jacksondesobral@hotmail.com>
2023-04-01 14:16:18 -0300

    bug_redirecionar_rota

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,34 @@
+import { useNavigate } from 'react-router-dom'
 import styles from '../pages/NewProject.module.css'
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+
+            body: JSON.stringify(project),
+
+        }).then((resp => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            history('/projects', { message: 'Projeto Criado com sucesso'})
+        })
+
+        ).catch(err => console.log(err))
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>
@@ -9,7 +36,7 @@ function NewProject(){
             </h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
             <p>Formulário</p>
-            <ProjectForm btnText='Criar Projeto'/>
+            <ProjectForm handleSubmit = {createPost} btnText='Criar Projeto'/>
         </div>
     )
 
